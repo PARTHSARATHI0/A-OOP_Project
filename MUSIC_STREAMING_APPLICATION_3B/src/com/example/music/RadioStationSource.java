9b401eabb62eb744dba27d65b528dfebf6229c0e
PARTHSARATHI K S <152023330+PARTHSARATHI0@users.noreply.github.com>
2024-09-09 23:30:05 +0530

    Delete MUSIC_STREAMING_APPLICATION_3B directory

--- MUSIC_STREAMING_APPLICATION_3B/src/com/example/music/RadioStationSource.java
+++
@@ -1,8 +0,0 @@
-package com.example.music;
-
-public class RadioStationSource implements MusicSource {
-    @Override
-    public void playMusic() {
-        System.out.println("Playing music from the radio station.");
-    }
-}
