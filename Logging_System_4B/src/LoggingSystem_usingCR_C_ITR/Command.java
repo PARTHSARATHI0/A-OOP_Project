619c9321969e9f48ff7f61286eeae3b96005d9dc
PARTHSARATHI K S <152023330+PARTHSARATHI0@users.noreply.github.com>
2024-10-31 18:23:34 +0530

    Delete Logging_System_4B directory

--- Logging_System_4B/src/LoggingSystem_usingCR_C_ITR/Command.java
+++
@@ -1,6 +0,0 @@
-package LoggingSystem_usingCR_C_ITR;
-
-//Command.java
-public interface Command {
- void execute(String message);
-}
--- Logging_System_4B/src/LoggingSystem_usingCR_C_ITR/LogLevel.java
+++
@@ -1,5 +0,0 @@
-package LoggingSystem_usingCR_C_ITR;
-
-public enum LogLevel {
- INFO, DEBUG, ERROR;
-}
